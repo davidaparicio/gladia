c8caf6a671757e36d19f5939168e38e19eea6db1
Thytu <valentin.de-matos@epitech.eu>
2022-09-11 17:41:37 +0000

    feat: logic for functional tests

---
+++ src/tests/utils/__init__.py
@@ -0,0 +1,3 @@
+from .get_models_to_test import get_models_to_test
+
+__all__ = ["get_models_to_test"]
---
+++ src/tests/utils/get_models_to_test.py
@@ -0,0 +1,16 @@
+import os
+
+from typing import List
+
+
+def get_models_to_test(path_to_task) -> List[str]:
+
+    if os.getenv("TEST_DEFAULT_MODELS_ONLY", None):
+        return set([""])
+
+    models = [model for model in os.listdir(path_to_task) if model[0] not in [".", "_"]]
+    models = [model for model in models if os.path.isdir(os.path.join(path_to_task, model))]
+    models = [model for model in models if os.path.isfile(os.path.join(path_to_task, model, f"{model}.py"))]
+
+
+    return set(models)
