18cf15aaa868b44b02d3fde37d8d92ad8197c1e0
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-09-11 20:27:36 +0000

    refactor(bg_removal): better convention for file + fixes

--- src/apis/image/image/background-removal-models/mobilenet/mobilenet.py
+++ src/apis/image/image/background-removal-models/mobilenet/mobilenet.py
@@ -1,11 +1,6 @@
-from typing import Tuple
-
-import onnxruntime as ort
-from gladia_api_utils.image_management import draw_segment
-from gladia_api_utils.io import _open
 from gladia_api_utils.model_management import download_model
-from numpy import asarray as as_nparray
-from numpy import ndarray
+from gladia_api_utils.OnnxBackgroundRemoval import OnnxBackgroundRemoval
+
 from PIL import Image
 
 MODEL_PATH = download_model(
@@ -14,35 +9,7 @@ MODEL_PATH = download_model(
     uncompress_after_download=False,
 )
 
-
-def run(image: Image, fast: bool = True) -> Tuple[Image.Image, ndarray]:
-    """
-    Call the model to return the image without its background
-
-    Args:
-        image (Image): Image to remove the background from
-        fast (bool): If True, uses a faster but less accurate model
-
-    Returns:
-        Image: Image without its background
-    """
-
-    del fast
-
-    INPUT_SIZE = 513
-
-    INPUT_TENSOR_NAME = "ImageTensor:0"
-
-    width, height = image.size
-    resize_ratio = 1.0 * INPUT_SIZE / max(width, height)
-    target_size = (int(resize_ratio * width), int(resize_ratio * height))
-    resized_image = image.convert("RGB").resize(target_size, Image.ANTIALIAS)
-
-    ort_sess = ort.InferenceSession(MODEL_PATH)
-    seg_map = ort_sess.run(None, {INPUT_TENSOR_NAME: [as_nparray(resized_image)]})[0][0]
-
-    return resized_image, seg_map
-
+onnx_bg_remover = OnnxBackgroundRemoval(model_path=MODEL_PATH)
 
 def predict(image: bytes) -> Image:
     """
@@ -55,9 +22,4 @@ def predict(image: bytes) -> Image:
         Image: Image without its background
     """
 
-    image = _open(image).convert("RGB")
-    resized_im, seg_map = run(image, True)
-
-    img = draw_segment(resized_im, seg_map)
-
-    return img
+    return onnx_bg_remover.remove_bg(image)
