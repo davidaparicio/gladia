189cbf6619900449339d1067c86852df69425672
Thytu <valentin.de-matos@epitech.eu>
2022-09-12 08:54:14 +0000

    feat(tests): IBasicTestsImageToImage interface

---
+++ src/apis/image/image/IBasicTestsImageToImage.py
@@ -0,0 +1,107 @@
+import abc
+
+
+class IBasicTestsImageToImage(metaclass=abc.ABCMeta):
+    """
+    Class to test the background removal endpoint
+    """
+
+    target_url = None
+
+    @abc.abstractmethod
+    def test_jpg_image_input_task(self, model: str) -> bool:
+        """
+        Test the background removal endpoint with a jpg image input
+
+        Args:
+            model (str): model to test
+
+        Returns:
+            bool: True if the test passed, False otherwise
+        """
+        
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def test_png_image_input_task(self, model: str) -> bool:
+        """
+        Test the background removal endpoint with a png image input
+
+        Args:
+            model (str): model to test
+
+        Returns:
+            bool: True if the test passed, False otherwise
+        """
+        
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def test_jpg_image_url_input_task(self, model: str) -> bool:
+        """
+        Test the background removal endpoint with a jpg image input retrieved from an url
+
+        Args:
+            model (str): model to test
+
+        Returns:
+            bool: True if the test passed, False otherwise
+        """
+        
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def test_png_image_url_input_task(self, model: str) -> bool:
+        """
+        Test the background removal endpoint with a png image input retrieved from an url
+
+        Args:
+            model (str): model to test
+
+        Returns:
+            bool: True if the test passed, False otherwise
+        """
+        
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def test_invalid_image_input_task(self, model: str) -> bool:
+        """
+        Test the background removal endpoint with an invalid image input
+
+        Args:
+            model (str): model to test
+
+        Returns:
+            bool: True if the test passed, False otherwise
+        """
+        
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def test_invalid_image_url_input_task(self, model: str) -> bool:
+        """
+        Test the background removal endpoint with an invalid image url input
+
+        Args:
+            model (str): model to test
+
+        Returns:
+            bool: True if the test passed, False otherwise
+        """
+        
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def test_empty_input_task(self, model: str) -> bool:
+        """
+        Test the background removal endpoint with an empty input
+
+        Args:
+            model (str): model to test
+
+        Returns:
+            bool: True if the test passed, False otherwise
+        """
+        
+        raise NotImplementedError
