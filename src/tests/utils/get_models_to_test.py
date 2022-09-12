0bfa5a9bfa468fd5eeda581ca04ad59fa19a00e1
Valentin De Matos <43698357+Thytu@users.noreply.github.com>
2022-09-12 09:04:17 +0200

    doc(test): background-removal FTs
    Co-authored-by: Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>

--- src/tests/utils/get_models_to_test.py
+++ src/tests/utils/get_models_to_test.py
@@ -2,7 +2,19 @@ import os
 from typing import List
 
 
-def get_models_to_test(path_to_task) -> List[str]:
+def get_models_to_test(path_to_task: str) -> List[str]:
+    """
+    Get a list of models to test for a given task. The list is obtained by
+    looking for all the directories in the task directory that contain a
+    `model.py` file.
+    If the environment variable `TEST_DEFAULT_MODELS_ONLY` is set, only the default models are returned.
+
+    Args:
+        path_to_task (str): Path to the task directory.
+
+    Returns:
+        List[str]: List of models to test.
+    """
 
     if os.getenv("TEST_DEFAULT_MODELS_ONLY", None):
         return set([""])
