82791ad024695f2f71c8a1aa44c894a90a1bd521
Valentin De Matos <43698357+Thytu@users.noreply.github.com>
2022-09-13 10:56:28 +0200

    doc(conftest): code smell in doc
    Co-authored-by: Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>

--- src/conftest.py
+++ src/conftest.py
@@ -2,10 +2,14 @@ from _pytest.config.argparsing import Parser
 
 
 def pytest_addoption(parser: Parser) -> None:
-    """Define custome command line arguments
+    """
+    Define custom command line arguments
 
     Args:
         parser (Parser): parser used by pytest to retrieve command line arguments
+        
+    Returns:
+        None
     """
 
     parser.addoption("--default-models-only", action="store_true", default=False)
