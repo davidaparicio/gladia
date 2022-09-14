57f49ded6bce8b7389bf99ab654db2838cb451c1
Thytu <valentin.de-matos@epitech.eu>
2022-09-14 08:45:34 +0000

    test(background-removal): retrieve tests from task_metadata & no longer use local file

--- src/apis/image/image/background-removal-models/test_background_removal.py
+++ src/apis/image/image/background-removal-models/test_background_removal.py
@@ -1,15 +1,20 @@
 import os
-
 import pytest
-from fastapi.testclient import TestClient
+import requests
+import tempfile
 
+from typing import Any, Dict
+from urllib.request import urlretrieve
+from fastapi.testclient import TestClient
 from apis.image.image.IBasicTestsImageToImage import IBasicTestsImageToImage
 from main import app
-from tests.constants import HOST_TO_EXAMPLE_STORAGE, PATH_TO_EXAMPLE_FILES
-from tests.utils import get_models_to_test
+from tests.constants import HOST_TO_EXAMPLE_STORAGE
+from tests.utils import get_models_to_test, get_inputs_to_test
 
 client = TestClient(app)
+
 models = get_models_to_test(os.path.split(__file__)[0])
+inputs_to_test = get_inputs_to_test(os.path.split(__file__)[0], ["image_url"])
 
 
 class TestBackgroundRemoval(IBasicTestsImageToImage):
@@ -21,85 +26,57 @@ class TestBackgroundRemoval(IBasicTestsImageToImage):
 
     @pytest.mark.mandatory
     @pytest.mark.parametrize("model", models)
-    def test_jpg_image_input_task(self, model: str) -> bool:
+    @pytest.mark.parametrize("inputs", inputs_to_test)
+    def test_image_input_task(self, model: str, inputs: Dict[str, Any]) -> bool:
         """
         Test the background removal endpoint with a jpg image input
 
         Args:
             model (str): model to test
+            inputs (Dict[str, Any]): input values to test
 
         Returns:
             bool: True if the test passed, False otherwise
         """
+
+        raw_img_data = requests.get(inputs["image_url"]).content
+
+        tmp_local_image_file = tempfile.NamedTemporaryFile(mode="wb", delete=False)
+        tmp_local_image_file.write(raw_img_data)
+
         response = client.post(
             url=self.target_url,
             params={"model": model} if model else {},
             files={
-                "image": open(os.path.join(PATH_TO_EXAMPLE_FILES, "test.jpg"), "rb")
+                "image": open(tmp_local_image_file.name, "rb")
             },
         )
 
-        assert response.status_code == 200
-
-    @pytest.mark.parametrize("model", models)
-    def test_png_image_input_task(self, model: str) -> bool:
-        """
-        Test the background removal endpoint with a png image input
-
-        Args:
-            model (str): model to test
-
-        Returns:
-            bool: True if the test passed, False otherwise
-        """
-        response = client.post(
-            url=self.target_url,
-            params={"model": model} if model else {},
-            files={
-                "image": open(os.path.join(PATH_TO_EXAMPLE_FILES, "test.png"), "rb")
-            },
-        )
+        tmp_local_image_file.close()
+        os.unlink(tmp_local_image_file.name)
 
         assert response.status_code == 200
 
     @pytest.mark.mandatory
     @pytest.mark.parametrize("model", models)
-    def test_jpg_image_url_input_task(self, model: str) -> bool:
+    @pytest.mark.parametrize("inputs", inputs_to_test)
+    def test_image_url_input_task(self, model: str, inputs: Dict[str, Any]) -> bool:
         """
         Test the background removal endpoint with a jpg image input retrieved from an url
 
         Args:
             model (str): model to test
+            inputs (Dict[str, Any]): input values to test
 
         Returns:
             bool: True if the test passed, False otherwise
         """
+
         response = client.post(
             url=self.target_url,
             params={"model": model} if model else {},
             data={
-                "image_url": f"{HOST_TO_EXAMPLE_STORAGE}/examples/image/image/background-removal/owl2.jpg"
-            },
-        )
-
-        assert response.status_code == 200
-
-    @pytest.mark.parametrize("model", models)
-    def test_png_image_url_input_task(self, model: str) -> bool:
-        """
-        Test the background removal endpoint with a png image input retrieved from an url
-
-        Args:
-            model (str): model to test
-
-        Returns:
-            bool: True if the test passed, False otherwise
-        """
-        response = client.post(
-            url=self.target_url,
-            params={"model": model} if model else {},
-            data={
-                "image_url": f"{HOST_TO_EXAMPLE_STORAGE}/examples/image/image/background-removal/owl2.png"
+                "image_url": inputs["image_url"]
             },
         )
 
@@ -116,6 +93,10 @@ class TestBackgroundRemoval(IBasicTestsImageToImage):
         Returns:
             bool: True if the test passed, False otherwise
         """
+
+        tmp_local_video_file = tempfile.NamedTemporaryFile(mode="wb", delete=False)
+        urlretrieve(f"{HOST_TO_EXAMPLE_STORAGE}/test/test.mp4", tmp_local_video_file.name)
+
         with pytest.raises(Exception):
             response = client.post(
                 url=self.target_url,
@@ -123,13 +104,17 @@ class TestBackgroundRemoval(IBasicTestsImageToImage):
                 files={
                     "image": (
                         "test.mp3",
-                        open(os.path.join(PATH_TO_EXAMPLE_FILES, "test.mp3"), "rb"),
+                        open(tmp_local_video_file.name, "rb"),
                         "audio/mpeg",
                     )
                 },
             )
 
             # assert response.status_code != 200 # TODO
+
+        tmp_local_video_file.close()
+        os.unlink(tmp_local_video_file.name)
+
 
     @pytest.mark.parametrize("model", models)
     def test_invalid_image_url_input_task(self, model: str) -> bool:
@@ -142,6 +127,7 @@ class TestBackgroundRemoval(IBasicTestsImageToImage):
         Returns:
             bool: True if the test passed, False otherwise
         """
+
         with pytest.raises(Exception):
             response = client.post(
                 url=self.target_url,
