cd2352799a8d31d74fd68ef14df0e2000ee71a98
jqueguiner <jean-louis.queguiner@gadz.org>
2022-10-13 13:34:43 +0000

    feat(geocoding): add geocoding

---
+++ src/apis/text/text/address_formating-models/geopy-formatter/geopy-formatter.py
@@ -0,0 +1,24 @@
+# format the input string as an clean adress using geopy
+
+from typing import Dict
+import geopy
+print(geopy.__file__)
+from geopy.geocoders import Nominatim
+
+def predict(address: str) -> Dict[str, str]:
+    """
+    Format the input string as an clean adress using geopy
+
+    Args:
+        address (str): The address to format
+    Returns:
+        Dict[str, str]: The formated address
+    """
+
+    geolocator = Nominatim(user_agent="gladia")
+
+    location = geolocator.geocode(address, addressdetails=True))
+
+    raw_prediction = location.raw
+
+    return {'prediction': location.address, 'raw_prediction': raw_prediction}
