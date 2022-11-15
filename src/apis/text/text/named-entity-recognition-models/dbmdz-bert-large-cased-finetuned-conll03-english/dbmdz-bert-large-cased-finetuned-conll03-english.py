b92aff58ff3d54f88ff6e945e5f259ec5318abb8
Thytu <vdmatos@gladia.io>
2022-11-15 19:39:23 +0000

    refactor: remove triton models
    Signed-off-by: Thytu <vdmatos@gladia.io>

--- src/apis/text/text/named-entity-recognition-models/dbmdz-bert-large-cased-finetuned-conll03-english/dbmdz-bert-large-cased-finetuned-conll03-english.py
+++ src/apis/text/text/named-entity-recognition-models/dbmdz-bert-large-cased-finetuned-conll03-english/dbmdz-bert-large-cased-finetuned-conll03-english.py
@@ -1,54 +1,54 @@
-import json
-from typing import Dict, List, Union
-
-import truecase
-from gladia_api_utils.triton_helper import (
-    TritonClient,
-    check_if_model_needs_to_be_preloaded,
-    data_processing,
-)
+import torch
+from transformers import AutoModelForTokenClassification, AutoTokenizer
 
 
-def predict(text: str) -> Dict[str, Union[List[Dict[str, Union[str, float]]], str]]:
+def predict(text: str) -> [(str, str)]:
     """
     Apply NER on the given task and return each token within the sentence associated to its label.
-
-    **Labels**:
-    O : Outside of a named entity
-    B-MISC : Beginning of a miscellaneous entity right after another miscellaneous entity
-    I-MISC : Miscellaneous entity
-    B-PER : Beginning of a person's name right after another person's name
-    I-PER : Person's name
-    B-ORG : Beginning of an organisation right after another organisation
-    I-ORG : Organisation
-    B-LOC : Beginning of a location right after another location
-    I-LOC : Location
-
-    Args:
-        text (str): The text to apply NER on
-
-    Returns:
-        Dict[str, Union[List[Dict[str, Union[str, float]]], str]]: The text with the NER applied (O, B-MISC, I-MISC, B-PER, I-PER, B-ORG, I-ORG, B-LOC, I-LOC)
+    **Labels**:\n
+    O : Outside of a named entity\n
+    B-MISC : Beginning of a miscellaneous entity right after another miscellaneous entity\n
+    I-MISC : Miscellaneous entity\n
+    B-PER : Beginning of a person's name right after another person's name\n
+    I-PER : Person's name\n
+    B-ORG : Beginning of an organisation right after another organisation\n
+    I-ORG : Organisation\n
+    B-LOC : Beginning of a location right after another location\n
+    I-LOC : Location\n
+    :param text: sentence to search the named entities in
+    :return: each token within the sentence associated to its label
     """
 
-    MODEL_NAME = "named-entity-recognition_dbmdz_bert-large-cased-finetuned-conll03-english_tensorrt_inference"
-    MODEL_SUB_PARTS = [
-        "named-entity-recognition_dbmdz_bert-large-cased-finetuned-conll03-english_tensorrt_model",
-    ]
-
-    client = TritonClient(
-        model_name=MODEL_NAME,
-        sub_parts=MODEL_SUB_PARTS,
-        output_name="output",
-        preload_model=check_if_model_needs_to_be_preloaded(MODEL_NAME),
+    tokenizer = AutoTokenizer.from_pretrained(
+        "dbmdz/bert-large-cased-finetuned-conll03-english"
+    )
+    model = AutoModelForTokenClassification.from_pretrained(
+        "dbmdz/bert-large-cased-finetuned-conll03-english"
     )
 
-    np_output = data_processing.text_to_numpy(truecase.get_true_case(text))
+    label_list = [
+        "O",  # Outside of a named entity
+        "B-MISC",  # Beginning of a miscellaneous entity right after another miscellaneous entity
+        "I-MISC",  # Miscellaneous entity
+        "B-PER",  # Beginning of a person's name right after another person's name
+        "I-PER",  # Person's name
+        "B-ORG",  # Beginning of an organisation right after another organisation
+        "I-ORG",  # Organisation
+        "B-LOC",  # Beginning of a location right after another location
+        "I-LOC",  # Location
+    ]
 
-    client.set_input(name="TEXT", shape=np_output.shape, datatype="BYTES")
+    # Bit of a hack to get the tokens with the special tokens
+    tokens = tokenizer.tokenize(tokenizer.decode(tokenizer.encode(text)))
+    inputs = tokenizer.encode(text, return_tensors="pt")
 
-    prediction_raw = client(np_output)
-    prediction_decode = prediction_raw[0].decode("utf8")
-    prediction = json.loads(prediction_decode)[0]
+    outputs = model(inputs)[0]
+    predictions = torch.argmax(outputs, dim=2)
 
-    return {"prediction": prediction, "prediction_raw": prediction_raw}
+    del tokenizer
+    del model
+
+    return [
+        (token, label_list[prediction])
+        for token, prediction in zip(tokens, predictions[0].tolist())
+    ]
