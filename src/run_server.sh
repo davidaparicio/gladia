f71c8e6b2f967d24ab9330c38ac9024b9e2f5175
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-11-21 16:25:01 +0100

    Update run_server.sh

--- src/run_server.sh
+++ src/run_server.sh
@@ -11,9 +11,9 @@ ln -s /usr/bin/python3.8 /usr/bin/python3
 service supervisor start
 
 # initialize the nltk database
-micromamba run -n server python -c "import nltk; nltk.download('punkt')"
+micromamba run -n server --cwd /app python -c "import nltk; nltk.download('punkt')"
 
-micromamba run -n server gunicorn main:app \
+micromamba run -n server --cwd /app gunicorn main:app \
 -b 0.0.0.0:${API_SERVER_PORT_HTTP} \
 --workers ${API_SERVER_WORKERS} \
 --worker-class uvicorn.workers.UvicornWorker \
