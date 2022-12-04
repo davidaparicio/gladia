a7cb937c31c6a7f2cdad6a93ab3dd693c63fa6c6
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-12-04 13:32:36 +0100

    Update runwayml-sd-15.py

--- src/apis/text/image/image-generation/runwayml-sd-15/runwayml-sd-15.py
+++ src/apis/text/image/image-generation/runwayml-sd-15/runwayml-sd-15.py
@@ -57,6 +57,8 @@ def predict(
     # TODO implement NSFW filter
     # {'sample': [<PIL.Image.Image image mode=RGB size=512x512 at 0x7F546A97A070>], 'nsfw_content_detected': [False]}
 
+
+    torch.cuda.empty_cache()
     if samples == 1:
         return images_list["images"][0]
     else:
