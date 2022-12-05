fa8f53c293958f7f1439750089da44bd87fe998e
jqueguiner <jean-louis.queguiner@gadz.org>
2022-12-05 14:11:17 +0000

    feat(spacy-ner): better warmup and output

--- src/apis/text/text/named-entity-recognition/spacy-en/spacy-en.py
+++ src/apis/text/text/named-entity-recognition/spacy-en/spacy-en.py
@@ -1,44 +1,35 @@
 from typing import Dict, Union
-
 import spacy
-import os
 
-try:
-    EN_CORE_WEB_LG = spacy.load("en_core_web_lg")
-except:
-    os.system("python -m spacy download en_core_web_lg")
-    EN_CORE_WEB_LG = spacy.load("en_core_web_lg")
+
+EN_CORE_WEB_LG = spacy.load("en_core_web_lg")
 
 
 def predict(text: str) -> Dict[str, Union[str, Dict[str, float]]]:
     """
     Apply NER on the given task and return each token within the sentence associated to its label.
 
-    **Labels**:\n
-    O : Outside of a named entity\n
-    B-MISC : Beginning of a miscellaneous entity right after another miscellaneous entity\n
-    I-MISC : Miscellaneous entity\n
-    B-PER : Beginning of a person's name right after another person's name\n
-    I-PER : Person's name\n
-    B-ORG : Beginning of an organisation right after another organisation\n
-    I-ORG : Organisation\n
-    B-LOC : Beginning of a location right after another location\n
-    I-LOC : Location\n
+    params:
+        text(str): sentence to search the named entities in
 
-    :param text: sentence to search the named entities in
-
-    :return: each token within the sentence associated to its label
+    return:
+        Dict[str, Union[str, Dict[str, float]]]: each token within the sentence associated to its label
     """
 
     document = EN_CORE_WEB_LG(text)
 
     prediction_raw = []
+    prediction = []
+    
     for entity in document.ents:
       extraction = {}
       extraction["first_index"] = entity.start_char
       extraction["last_index"] = entity.end_char
       extraction["name"] = entity.label_
       extraction["content"] = entity.text
+
+      prediction.append({"text": entity.text, "label": entity.label_})
+
       prediction_raw.append(extraction)
 
-    return {"prediction": prediction_raw, "prediction_raw": prediction_raw}
+    return {"prediction": prediction, "prediction_raw": prediction_raw}
--- src/warm_up.py
+++ src/warm_up.py
@@ -1,6 +1,15 @@
-def main():
-    pass
+import os
+import nltk
 
+def main():
+    nltk.download('punkt')
+
+    spacy_warmup_list = ["en_core_web_lg"]
+    for spacy_model in spacy_warmup_list:
+        try:
+            os.system("python -m spacy download {}".format(spacy_model))
+        except:
+            pass
 
 if __name__ == "__main__":
     main()
