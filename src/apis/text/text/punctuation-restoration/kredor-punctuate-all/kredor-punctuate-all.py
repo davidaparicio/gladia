9d2e73067feef78fc7d7dccdddb330aa74f9105d
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-12-06 19:55:03 +0100

    fix(953-punctuation): fix punctuation for multiple sentences (#954)
    * fix(punctuation): fix punctuation restoration
    
    * style(docstring): fix docstring for punctuation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/apis/text/text/punctuation-restoration/kredor-punctuate-all/kredor-punctuate-all.py
+++ src/apis/text/text/punctuation-restoration/kredor-punctuate-all/kredor-punctuate-all.py
@@ -1,21 +1,36 @@
-from typing import Dict
+from typing import Dict, List, Union
 
+import truecase
 from deepmultilingualpunctuation import PunctuationModel
 
 model = PunctuationModel(model="kredor/punctuate-all")
 
 
-def predict(sentence: str) -> Dict[str, str]:
+def predict(sentence: str) -> Dict[str, Union[str, List[Dict[str, Union[str, float]]]]]:
     """
     Format the input sentence as a punctuation-restored sentence
 
     Args:
         sentence (str): The input string to be punctuated
     Returns:
-        Dict[str, str]: The punctuation-restored string
+        Dict[str, Union[str, List[Dict[str, Union[str, float]]]]]: The punctuation-restored string
     """
 
-    return {
-        "prediction": model.restore_punctuation(sentence),
-        "raw_prediction": model.predict(model.preprocess(sentence)),
-    }
+    original_sentences = sentence.split(".")
+    prediction = ""
+    prediction_raw = list()
+
+    for _, original_sentence in enumerate(original_sentences):
+        restored_sentence = truecase.get_true_case(
+            (model.restore_punctuation(original_sentence) + ".").strip()
+        )
+        prediction += restored_sentence
+        prediction_raw.append(
+            {
+                "original_sentence": original_sentence,
+                "restored_sentence": restored_sentence,
+                "scores": model.predict(model.preprocess(sentence)),
+            }
+        )
+
+    return {"prediction": prediction, "prediction_raw": prediction_raw}
