700192d82b7fc6cf19d9efbe87a6fc2198a9eaab
jqueguiner <jean-louis.queguiner@gadz.org>
2022-12-10 21:09:01 +0000

    feat(video-classification-kinetics): add video-classification

---
+++ src/apis/video/text/action-classifications/kinetics/kinetics.py
@@ -0,0 +1,34 @@
+from typing import Dict, Union, List
+from transformers import pipeline
+from gladia_api_utils.file_management import input_to_files
+from gladia_api_utils import SECRETS
+
+
+
+
+@input_to_files
+def predict(video: bytes, top_k: int = 5,  model_version: str = "MCG-NJU-videomae-base-finetuned-kinetics") -> Dict[str, Union[str, List[Dict[str, Union[str, float]]]]]:
+    """
+    Predicts the class of a video using the Kinetics-600 dataset.
+
+    Args:
+        video (bytes): The video to classify.
+        top_k (int): The number of classes to return.
+        model_version (str): The version of the model to use.
+
+    Returns:
+        Dict[str, Union[str, List[Dict[str, Union[str, float]]]]]: The prediction and the raw prediction.
+    """
+
+
+    video_classifier = pipeline(
+        "video-classification",
+        model=model_version,
+        frame_sampling_rate=4,
+        use_auth_token=SECRETS["HUGGINGFACE_ACCESS_TOKEN"]
+        )
+
+    prediction_raw = video_classifier(video)
+
+    return {"prediction": prediction_raw[0]["label"], "prediction_raw": prediction_raw}
+
--- src/warm_up.py
+++ src/warm_up.py
@@ -1,6 +1,7 @@
 import os
 
 import nltk
+import importlib
 
 
 def main():
@@ -9,9 +10,12 @@ def main():
     spacy_warmup_list = ["en_core_web_lg"]
     for spacy_model in spacy_warmup_list:
         try:
-            os.system("python -m spacy download {}".format(spacy_model))
-        except:
-            pass
+            importlib.import_module(spacy_model)
+        except ModuleNotFoundError:
+            try:
+                os.system("python -m spacy download {}".format(spacy_model))
+            except Exception as e:
+                print("Error while downloading spacy model: {}".format(e))
 
 
 if __name__ == "__main__":
