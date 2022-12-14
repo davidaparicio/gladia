1fa9226decf093c5ba6443dd95831311e7aff213
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-12-14 13:42:36 +0000

    refactor(prepare): optimize prepare

--- src/prepare.py
+++ src/prepare.py
@@ -4,14 +4,46 @@ import nltk
 
 
 def main():
-    nltk.download("punkt")
-
+    nltk_warmup_list = ["punkt"]
     spacy_warmup_list = ["en_core_web_lg"]
+
+    download_nltk_data(nltk_warmup_list)
+    download_spacy_model(spacy_warmup_list)
+
+
+def download_nltk_data(nltk_warmup_list: list) -> None:
+    """
+    Download nltk data
+
+    Args:
+        nltk_warmup_list (list): List of nltk data to download
+
+    Returns:
+        None
+    """
+    for tokenizer in nltk_warmup_list:
+        try:
+            nltk.data.find(f"tokenizers/{tokenizer}")
+        except LookupError:
+            nltk.download(tokenizer)
+
+
+def download_spacy_model(spacy_warmup_list: list) -> None:
+    """
+    Download spacy model
+
+    Args:
+        spacy_warmup_list (list): List of spacy model to download
+
+    Returns:
+        None
+    """
+
     for spacy_model in spacy_warmup_list:
         try:
+            __import__(spacy_model)
+        except ImportError:
             os.system("python -m spacy download {}".format(spacy_model))
-        except:
-            pass
 
 
 if __name__ == "__main__":
